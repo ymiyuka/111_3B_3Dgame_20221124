f198cf6240a376ef3c7657d58245a616d86917bc
ymiyuka <a9748397531@gmail.com>
2022-12-01 18:07:46 +0800

    20221201-進度-1

---
+++ 111_3B_3Dgame_20221124/Assets/Scripts/DialogueData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace yo
+{
+    /// <summary>
+    /// 對話資料
+    /// </summary> 
+    [CreateAssetMenu(menuName = "YO/Dialogue Data", fileName ="New Dialogue Data")]
+    public class DialogueData : ScriptableObject
+    {
+        [Header("對話者名稱")]
+        public string DialogueName;
+        [Header("對話者內容"), TextArea(2,10)]
+        public string[] DialogueContents;
+    }
+}
+
